4e5d160295af51eb9670f5560435d872f76a390c
Alexander Mills <alex@oresoftware.com>
2026-09-05 17:46:04 -0500

    test(DEN-3587): retain canonical and extension Go layouts

--- scripts/validate-client-layout.sh
+++ scripts/validate-client-layout.sh
@@ -14,6 +14,7 @@ required=(
   clients/rust/Cargo.toml
   clients/wasm/Cargo.toml
   clients/java/pom.xml
+  clients/golang/go.mod
   clients/go/go.mod
   clients/python/pyproject.toml
   clients/ruby/hhm_client.gemspec
